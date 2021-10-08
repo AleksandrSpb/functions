15df670dfd3fa3236cefcb7fb8c9995126a94663
AleksandrSpb <aleksandr_l@inbox.ru>
2021-10-08 11:36:44 +0300

    функции задачи номер 2 решена

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const dog = 'https://pictures.s3.yandex.net/frontend-developer/functions/dog-1.jpg';
 
 function setDog() {
+    const dog = 'https://pictures.s3.yandex.net/frontend-developer/functions/dog-2.jpg';
     document.querySelector('.dog').style.backgroundImage = `url(${dog})`;
 }
 
