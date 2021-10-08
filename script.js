434d5fb583e6dd12e2034559600546fb0eeca204
AleksandrSpb <aleksandr_l@inbox.ru>
2021-10-08 11:24:28 +0300

    функции задачи номер 2

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-const dog1 = 'https://pictures.s3.yandex.net/frontend-developer/functions/dog-1.jpg';
-function consoleDog() {
-    const dog2 = 'https://pictures.s3.yandex.net/frontend-developer/functions/dog-2.jpg';
-    console.log(dog1);
-    console.log(dog2);
+const dog = 'https://pictures.s3.yandex.net/frontend-developer/functions/dog-1.jpg';
+
+function setDog() {
+    document.querySelector('.dog').style.backgroundImage = `url(${dog})`;
 }
 
-consoleDog();
+document.querySelector('button').addEventListener('click', setDog);
---
+++ script_task_1.js
@@ -0,0 +1,8 @@
+const dog1 = 'https://pictures.s3.yandex.net/frontend-developer/functions/dog-1.jpg';
+function consoleDog() {
+    const dog2 = 'https://pictures.s3.yandex.net/frontend-developer/functions/dog-2.jpg';
+    console.log(dog1);
+    console.log(dog2);
+}
+
+consoleDog();
