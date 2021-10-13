1ffa458b72cb97eedde078b67924b1a3b5b7a412
AleksandrSpb <aleksandr_l@inbox.ru>
2021-10-13 12:13:22 +0300

    задача 5 решена стрелочные функции

---
+++ script5.js
@@ -0,0 +1,10 @@
+const wannaBeArrow = () => {
+    console.log('Сделайте меня стрелочной, пожалуйста');
+};
+
+const wannaBeArrowToo =  (developerName) => {
+    console.log(`${developerName}, сделайте меня стрелочной, пожалуйста`);
+};
+
+wannaBeArrow();
+wannaBeArrowToo('Стас Басов');
