4fe3a0b13e08f782120159a34d5d9d2fb0fa6b73
AleksandrSpb <aleksandr_l@inbox.ru>
2021-10-15 17:44:39 +0300

    задача 7

---
+++ script7.js
@@ -0,0 +1,15 @@
+function makeBeastArray(a, b, c) {
+    if (a === undefined) {
+        a = 'Коала';
+    }
+
+    if (b === undefined) {
+        b = 'Вомбат';
+    }
+
+    if (c === undefined) {
+        c = 'Ленивец';
+    }
+
+    return [a, b, c];
+}
